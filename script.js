3f733597508cfb58581f2c9589304f45005e254a
SRINIVAS2030 <djsrinivas2030@gmail.com>
2024-02-28 02:27:34 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const counter = document.querySelector(".counter-number");
+async function updateCounter() {
+    let response = await fetch(
+        "https://avzimaaf36onnnyqivzq7btwvq0hnlof.lambda-url.ap-south-1.on.aws/"
+    );
+    let data = await response.json();
+    counter.innerHTML = `Views: ${data}`;
+}
+updateCounter();
