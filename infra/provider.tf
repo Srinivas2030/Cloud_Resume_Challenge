692ca88996507c235d31083de541181893d0b5ab
SRINIVAS2030 <djsrinivas2030@gmail.com>
2024-02-28 17:54:51 +0530

    modifyHome

---
+++ infra/provider.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      version = ">=4.9.0"
+      source  = "hashicorp/aws"
+    }
+
+  }
+
+}
+
+
+
+provider "aws" {
+   region     = "ap-south-1"
+}
